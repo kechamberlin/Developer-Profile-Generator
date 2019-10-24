2ce8bb253c9f8b0f2a53f00652d6ccfd4de9f0b5
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-10-23 19:44:12 -0700

    Added text to README file.  Replaced previous code with code similar to Team-Profile assignment.  Deleted generateHTML.js file and added an index.html file in its place.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Developer-Profile-Generator
+
+An application where the user will input information, and a basic Developer Profile will be generated from his or her response.
+
+Upon entering NodeJS, the user will be prompted for their name, location, email, and GitHub username.
+
+Once completed, the user's information will be generated onto an HTML page.
--- index.js
+++ index.js
@@ -1,58 +1,104 @@
 const fs = require("fs");
-const axios = require("axios");
 const inquirer = require("inquirer");
 
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your name?"
-        },
-        {
-            type: "input",
-            name: "location",
-            message: "Where are you from?"
-        },
-        {
-            type: "input",
-            name: "color",
-            message: "What is your favorite color?"
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Enter your GitHub Username"
-        }
-    ])
+// ====== Developer ======
 
-        .then(function ({ username }) {
-            let queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-            axios.get(queryUrl).then(response => {
-                console.log(response.data[0].name);
-                const repositoryNames = response.data.map(repository => repository.name);
-                console.log(repositoryNames);
-            })
-        })
+class Developer {
+    constructor(name, location, email, github) {
+        this.name = name;
+        this.location = location;
+        this.email = email;
+        this.github = github;
+    }
+    getName() {
+        console.log(this.name);
+    }
+    getLocation() {
+        console.log(this.location);
+    }
+    getEmail() {
+        console.log(this.email);
+    }
+    getGitHub() {
+        return this.github;
+    }
+    printInfo() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Location: ${this.location}`);
+        console.log(`Email: ${this.email}`);
+        console.log(`GitHub Username: ${this.github}`);
 
-        .then(function ({ location }) {
-            // Couldn't get a real google maps api, so this placeholder is used instead
-            let queryUrl = `"https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"`;
-        });
+    }
+    // This method will convert input data into a string that will be then later be printed onto an HTML page via a function.
+    buildHTMLString() {
+        return `
+        <h1>Name: ${this.name}</h1>
+        <h1>Location: ${this.location}</h1>
+        <h1>Email: ${this.email}</h1>
+        <h1>GitHub Username: ${this.github}</h1>
+        `
+    }
+}
+
+Developer.prompt = () => {
+    return inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+        type: "input", 
+        message: "What is your full name?",
+        name: "fullname"
+      },
+      {
+        type: "input",
+        message: "What is your location?",
+        name: "location"
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+      },
+      {
+          type: "input",
+          message: "What is your GitHub username?",
+          name: "github"
+    },
+    ]).then(answers => {
+        return new Developer(answers.fullname, answers.location, answers.email, answers.github);
+    })
 }
 
 
+// Generate the HTML page
+const generatePage = (developer) => {
+    const wholePage = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        ${developer.buildHTMLString()}
+    </body>
+    </html>`
+    console.log(wholePage);
 
+    fs.writeFile("index.html", wholePage, function(err) {
 
-promptUser()
-    .then(function (data) {
-        const html = generateHTML(data);
-        return writeFileAsync("generateHTML.html", html);
-    })
-    .then(function () {
-        console.log("Successfully wrote to index.html");
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success!");
+      });
+}
+
+async function main() {
+    const developer = await Developer.prompt();    
+    generatePage(developer);
+}
+
+main();
